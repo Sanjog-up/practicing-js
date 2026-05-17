e7bcd50cfbfeb7c43bfeda1206595621aebef621
Sanjog Aale <sanjogm940@gmail.com>
2026-05-18 03:03:09 +0545

    destroying elements

---
+++ destroyer.js
@@ -0,0 +1,15 @@
+function destroyer(arr, args){
+
+    return arr.filter(el=> !args.includes(el))
+
+}
+
+console.log(destroyer([2,3,4,5],[2,3,4,6,8]));
+
+function destroyer(arr, ...args){
+const flatArgs = args.flat();
+    return arr.filter(el=> !flatArgs.includes(el))
+
+}
+
+console.log(destroyer([1,9,2,3,4,5],[2,3,4,6,8],[2,3,5,7]));
